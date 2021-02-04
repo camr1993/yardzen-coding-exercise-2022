8aa4a5167442666a4dd22305d30cbefbe37d1d97
Cameron Ratliff <cratliff@colgate.edu>
2021-02-04 15:34:00 -0500

    done with lesson

---
+++ src/TextField.tsx
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react'
+
+interface Person {
+  firstName: string
+  lastName: string
+}
+
+interface Props {
+  text: string
+  ok?: boolean
+  i?: number
+  fn?: (bob: string) => string
+  // obj: {
+  //   f1: string
+  // }
+  person: Person
+  // prettier-ignore
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+interface TextNode {
+  text: string
+}
+
+export const TextField: React.FC<Props> = ({ handleChange }) => {
+  const [count, setCount] = useState<TextNode>({ text: 'hello' })
+  const inputRef = useRef<HTMLInputElement>(null)
+  const divRef = useRef<HTMLDivElement>(null)
+
+  return (
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
+    </div>
+  )
+}
