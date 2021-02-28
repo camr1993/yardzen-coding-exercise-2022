a2ad75ec71832cc011fe258d96f1b632622a8103
Cameron Ratliff <cratliff@colgate.edu>
2021-02-28 18:22:42 -0500

    added buttons to summary component

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -19,6 +19,16 @@ const StyledOrderSummary = styled.div`
       display: flex;
       justify-content: space-between;
       margin-bottom: 20px;
+
+      .budget-line {
+        display: flex;
+        align-items: center;
+
+        .change {
+          margin-left: 7px;
+          padding: 4px 7px;
+        }
+      }
     }
 
     hr {
@@ -37,6 +47,31 @@ const StyledOrderSummary = styled.div`
     .bi-x-circle {
       color: #f32020;
       margin-right: 10px;
+    }
+
+    button {
+      color: #f5f4f4;
+      border-radius: 0.25rem;
+      background-color: #667eea;
+      cursor: pointer;
+      box-shadow: 0 1px 3px 0 rgb(0 0 0 / 10%), 0 1px 2px 0 rgb(0 0 0 / 6%);
+      border: none;
+      transition: background-color 0.3s ease;
+
+      &:hover {
+        background-color: #89a4f5;
+      }
+    }
+    button[disabled='disabled'],
+    button:disabled {
+      cursor: default;
+      background-color: grey;
+    }
+
+    .submit {
+      margin-top: 25px;
+      padding: 7px 15px;
+      font-size: 18px;
     }
   }
 `
@@ -72,15 +107,18 @@ const OrderSummary: React.FC<OrderSummaryProps> = ({ budget, cart }) => {
       <h2>Order Summary</h2>
       <div className="summary">
         <div className="summary-item">
-          <div>Budget</div>
+          <div className="budget-line">
+            <div>Budget</div>
+            <button className="change">Change</button>
+          </div>
           <div>${formatMoney(Number(budget))}</div>
         </div>
         <div className="summary-item">
-          <div>Price (Upper Limit)</div>
+          <div>Price (Upper Range)</div>
           <div>${formatMoney(upperPrice)}</div>
         </div>
         <div className="summary-item">
-          <div>Price (Lower Limit)</div>
+          <div>Price (Lower Range)</div>
           <div>${formatMoney(lowerPrice)}</div>
         </div>
         <hr />
@@ -133,6 +171,13 @@ const OrderSummary: React.FC<OrderSummaryProps> = ({ budget, cart }) => {
             <div>Enough to Cover Your Items!</div>
           </div>
         )}
+        <button
+          className="submit"
+          type="button"
+          disabled={Number(budget) < lowerPrice || cart.length === 0}
+        >
+          Submit
+        </button>
       </div>
     </StyledOrderSummary>
   )
